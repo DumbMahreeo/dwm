b970c4f6523fd02b4925dcbcf661815c7b9df273
DumbMahreeo <dumbmahreeo@github.com>
2022-02-13 01:18:10 +0100

    Added flameshot binding

--- config.h
+++ config.h
@@ -9,23 +9,23 @@
  * */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const int userbh             = 30;       /* bar height */
 static const int gappx              = 15;       /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "adove-source-code-pro:size=12";
+static const char *fonts[]          = { dmenufont };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-//static const char col_cyan[]        = "#ff0000";
-static const char col_cyan[]        = "#005578";
+static const char col_select[]        = "#212121";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_select, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_select,  col_gray2},
 };
 
 /* tagging */
@@ -50,7 +50,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "Tile",      dwindle },    /* first entry is default */
+	{ "",          dwindle },    /* first entry is default */
 	{ "Full",      monocle },
 };
 
@@ -67,15 +67,17 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "--fn", dmenufont, "--nb", col_gray1, "--nf", col_gray3, "--sb", col_cyan, "--sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "--fn", dmenufont, "--nb", col_gray1, "--nf", col_gray3, "--sb", col_select, "--sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *browser[]  = { "perl", "-e", "$a=`xdg-settings get default-web-browser`; $a =~ /\\.desktop/; exec $`", NULL };
+static const char *flameshot[]  = { "flameshot", "gui", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,            {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,            {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,            {.v = browser } },
+	{ MODKEY,                       XK_Print,  spawn,            {.v = flameshot } },
 	{ MODKEY,                       XK_b,      togglebar,        {0} },
 	{ MODKEY,                       XK_j,      focusstack,       {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,       {.i = -1 } },
